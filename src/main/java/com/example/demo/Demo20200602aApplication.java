3cf2b6689f60618b924bd8152e356063f0d923dc
hepeng <914535402@qq.com>
2020-06-11 14:25:05 +0800

    132

---
+++ src/main/java/com/example/demo/Demo20200602aApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo20200602aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo20200602aApplication.class, args);
+	}
+
+}
